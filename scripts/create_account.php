7cd4f996c35daa91a0574b7a8297feb094102ac2
Connor Hansen <connorhansen@github.com>
2018-04-27 21:59:49 -0400

    Added X icon if username is taken

--- scripts/create_account.php
+++ scripts/create_account.php
@@ -2,17 +2,7 @@
 <?php session_start(); ?>
 
 <?php 
-
-
-//
-// next version add check if note already exists -> and prompt user to overwrite if so
-//
-
-//
-// connect to database using session variables ( user/pass )
-//$mysqli = mysqli_connect("localhost", "root", "root", "final_project_db") or die(mysql_error());
-include ('db_connect.php');
-
+    include ('db_connect.php');
 
     //echo ("All form fields filled");
     $username = $_POST['username'];
@@ -22,27 +12,23 @@ include ('db_connect.php');
     $sql = "SELECT username FROM `auth_users` WHERE username = '$username'";
     $result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
 
+    // user exists
     if (mysqli_num_rows($result) >= 1) {
+        // update modal title
         echo "<script>$('#loader-modal-title').text('username taken!');</script>";
-        
-        // echo '<script>alert("Username is taken!");</script>';
+        // TODO replace spinner with 'X' icon
+        echo '<script>$(\'#spinner\').replaceWith(\'<img src="assets/icons/cancel.png" width="50px" alt="Checked Icon" />\');</script>';
     }
-        // if (mysqli_query($mysqli, $sql)) {
-        //     mysqli_close($mysqli);
-            
-        //     // echo '<script>$("#loader-modal-title").text("Account Created!");
-        //     // $("#spinner").replaceWith("<img src="assets/icons/checked.png" width="50px" alt="Checked Icon" />");</script>';
-        //     //echo '<script>location.href = "../dashboard.php";</script>';
-        //     // echo "New record created successfully";
-        // } 
-        else {
-            $sql = "INSERT INTO `auth_users` (username, password) VALUES ('$username', '$password')";
-            if (mysqli_query($mysqli, $sql)) {
-                echo "<script>$('#loader-modal-title').text('Account Created!');</script>";
-                // echo "<script>$('.modal-container').css('display','none');</script>";
-                // echo '<script>alert("Account Created");</script>';
-            }
+    // user doesn't exist
+    else {
+        // create user
+        $sql = "INSERT INTO `auth_users` (username, password) VALUES ('$username', '$password')";
+        if (mysqli_query($mysqli, $sql)) {
+            // update modal title
+            echo "<script>$('#loader-modal-title').text('Account Created!');</script>";
+            // echo "<script>$('.modal-container').css('display','none');</script>";
         }
+    }
 //
 // close connection
 mysqli_close($mysqli);
